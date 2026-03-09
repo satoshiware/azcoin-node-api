5be7bc27d9174223d50a8fdd4e0a436fb22bea10
bcraypo <bcraypo@gmail.com>
2026-03-09 11:07:37 -0700

    Creating latest Release v0.1.4

--- rebuild.ps1
+++ rebuild.ps1
@@ -8,9 +8,14 @@ git push origin v0.1.4
 $SHA = (git rev-parse --short HEAD).Trim()
 
 # Build once, tag many (v0.1.4 + stable + sha; optionally latest)
-docker build `
-docker build -t ghcr.io/satoshiware/azcoin-node-api:sha-$SHA `
-docker build -t ghcr.io/satoshiware/azcoin-node-api:latest .`
+$SHA = (git rev-parse --short HEAD).Trim()
 
+docker build `
+  -t ghcr.io/satoshiware/azcoin-node-api:v0.1.4 `
+  -t ghcr.io/satoshiware/azcoin-node-api:sha-$SHA `
+  -t ghcr.io/satoshiware/azcoin-node-api:latest `
+  .
+
+docker push ghcr.io/satoshiware/azcoin-node-api:v0.1.4
 docker push ghcr.io/satoshiware/azcoin-node-api:sha-$SHA
 docker push ghcr.io/satoshiware/azcoin-node-api:latest
