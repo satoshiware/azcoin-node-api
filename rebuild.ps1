955cc74b6c99bb497c7ac1ee1bfcf67458370462
bcraypo <bcraypo@gmail.com>
2026-04-16 15:24:30 -0700

    Updating fixes to expose more translator data. 0.1.7-r1

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,9 +1,9 @@
 git status
 git add -A
-git commit -m "Major update. Added bare metal linux install removed pool monitoring and added new translator monitoring 0.1.7"
-git tag v0.1.7
+git commit -m "Updating fixes to expose more translator data. 0.1.7-r1"
+git tag v0.1.7-r1
 git push origin main
-git push origin v0.1.7
+git push origin v0.1.7-r1
 
 $SHA = (git rev-parse --short HEAD).Trim()
 
